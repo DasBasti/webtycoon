126d5d64483d0872ec0a53bd98dd26655025f498
einfachsaufen <einfachsaufen@78ba09db-4829-0410-bc9e-e3a0c301aae7>
2007-03-14 12:28:11 +0000

    working, too!

--- trunk/client/js/login.js
+++ trunk/client/js/login.js
@@ -4,3 +4,8 @@ function login(user,pass){
 	//Sende einen Loginrequest an den Server...
 	sende("Auth::Login",[user,pass]);
 }
+
+function logout() {
+	// logge aus
+	sende("Auth::Logout",sessionid);
+}
