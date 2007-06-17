3b8b13a12d9c9923845842de04558408dee5ce79
einfachsaufen <einfachsaufen@78ba09db-4829-0410-bc9e-e3a0c301aae7>
2007-06-17 14:24:26 +0000

    zum laufen gebracht

--- client/js/login.js
+++ client/js/login.js
@@ -3,6 +3,7 @@
 function login(user,pass){
 	//Sende einen Loginrequest an den Server...
 	sende("Auth::Login",[user,pass]);
+	window.defaultStatus="Login in progress";
 }
 
 function logout() {
--- client/res/fields.php
+++ client/res/fields.php
@@ -1,17 +1,18 @@
 <?
+$url="http://localhost/client/";
 $bfield = array();
-$bfield[0]="img/wasser.png";
-$bfield[1]="img/grass.png";
-$bfield[2]="img/stein.png";
+$bfield[0]=$url."img/wasser.png";
+$bfield[1]=$url."img/gras.png";
+$bfield[2]=$url."img/stein.png";
 
 $build = array();
-$build[0]="img/empty.png";
-$build[1]="img/zelt.png";
-$build[2]="img/feuer.png";
-$build[3]="img/holz.png";
-$build[4]="img/wohnwagen.png";
-$build[5]="img/strasse.png";
-$build[6]="img/spielplatz.png";
-$build[7]="img/spielhalle.png";
-$build[8]="img/internetcafe.png";
+$build[0]=$url."img/empty.png";
+$build[1]=$url."img/zelt.png";
+$build[2]=$url."img/feuer.png";
+$build[3]=$url."img/holz.png";
+$build[4]=$url."img/wohnwagen.png";
+$build[5]=$url."img/strasse.png";
+$build[6]=$url."img/spielplatz.png";
+$build[7]=$url."img/spielhalle.png";
+$build[8]=$url."img/internetcafe.png";
 ?>
